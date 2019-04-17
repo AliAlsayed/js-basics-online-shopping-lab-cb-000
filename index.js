eadcfea0fbe179cc52ba06a7070834fad7be8bf9
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-17 09:34:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,7 +55,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  total = total()
   if(!cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
+  return `your t`
 }
