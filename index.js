866d71459c33d22a7ea0d34e8c4addeba63e6fd5
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-17 09:28:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,7 +45,14 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var i = 0; i < cart.length; i++){
+    if(cart[i].itemName === item){
+      cart.splice(i, 1);
+      return cart;
+    }
+    return "That item is not in your cart."
+  }
+  return total_price;
 }
 
 function placeOrder(cardNumber) {
