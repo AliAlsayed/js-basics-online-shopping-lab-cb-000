55d74bdac867e170337077fb8b5bdc8ca7600b16
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-17 09:37:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,9 +55,13 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  total = total()
+  total = 0
   if(!cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
+  }
+  
+  for(var i = 0; i < cart.length; i++){
+    total += cart[i].itemPrice
   }
   cart = []
   return `Your total cost is $${total}, which will be charged to the card ${cardNumber}`
